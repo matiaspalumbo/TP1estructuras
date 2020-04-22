0ece489214c55b0766c2e8eb46f989189c9ff5f5
inescipullo <64146923+inescipullo@users.noreply.github.com>
2020-04-22 13:36:52 -0300

    gdc listos

--- gdclist.c
+++ gdclist.c
@@ -5,14 +5,16 @@
 // agregar funcion intercambiar (nodos)
 // analizar utilidad de función recorrer
 
-
-GList glist_crear() {
+GList gdclist_crear() {
   return NULL;
 }
 
-void glist_destruir(GList lista) {
-  GNodo *ultimo = lista->ant;
-  ultimo->sig = NULL;
+void gdclist_destruir(GList lista) {
+  if (lista != NULL) {
+    GNodo *nodo = lista;
+    for (; nodo->sig != lista; nodo = nodo->sig);
+    nodo->sig = NULL;
+  }
   GNodo *nodoAEliminar;
   while (lista != NULL) {
     nodoAEliminar = lista;
@@ -21,31 +23,70 @@ void glist_destruir(GList lista) {
   }
 }
 
-int glist_vacia(GList lista) {
+int gdclist_vacia(GList lista) {
   return lista == NULL;
 }
 
-GList glist_agregar_final(GList lista, void * dato) {
-  GNodo *nuevoNodo = malloc(sizeof(GNodo));
-  nuevoNodo->dato = dato;
-  nuevoNodo->sig = lista;
-  GNodo *ultimo = lista->ant;
-  if (lista != NULL)
-    GNodo *ultimo = lista->ant;
-  if (lista == NULL)
-    lista = nuevoNodo;
-
-  return lista;
-}
-
-GList glist_agregar_inicio(GList lista, void * dato) {
-  GNodo * nuevoNodo = malloc(sizeof(GNodo));
-  nuevoNodo->dato = dato;
-  nuevoNodo->sig = lista;
+GNodo *gdclist_agregar_inicio(GList lista, void *dato) {
+  GNodo *nuevoNodo = malloc(sizeof(GNodo));  
+  if (lista == NULL) {
+    nuevoNodo->dato = dato;
+    nuevoNodo->sig = lista;
+    nuevoNodo->ant = lista;
+  } else {
+    nuevoNodo->dato = dato;
+    nuevoNodo->sig = lista;
+    lista->ant = nuevoNodo;
+    GNodo *nodo = lista;
+    for (; nodo->sig != lista; nodo = nodo->sig);
+    /* ahora 'nodo' apunta al ultimo elemento en la lista */
+    nodo->sig = nuevoNodo;
+    nuevoNodo->ant = nodo;
+  }
   return nuevoNodo;
 }
 
-void glist_recorrer(GList lista, FuncionVisitante visit) {
-  for (GNodo * nodo = lista; nodo != NULL; nodo = nodo->sig)
-    visit(nodo->dato);
+GNodo *gdclist_agregar_final(GList lista, void *dato) {
+  if (lista == NULL) {
+    lista = malloc(sizeof(GNodo));
+    lista->dato = dato;
+    lista->sig = lista;
+    lista->ant = lista;
+  } else {
+    GNodo *nuevoNodo = malloc(sizeof(GNodo));
+    nuevoNodo->dato = dato;
+    nuevoNodo->sig = lista;
+    lista->ant = nuevoNodo;
+    GNodo *nodo = lista;
+    for (; nodo->sig != lista; nodo = nodo->sig);
+    /* ahora 'nodo' apunta al ultimo elemento en la lista */
+    nodo->sig = nuevoNodo;
+    nuevoNodo->ant = nodo;
+  }
+  return lista;
+}
+
+GNodo *gdclist_intercambiar(GNodo *lista, int posicion1, int posicion2) {
+  GNodo *inicio = lista;
+  GNodo *nodoA, *nodoB;
+  int menor = (posicion1 < posicion2) ? posicion1 : posicion2;
+  int mayor = (posicion1 < posicion2) ? posicion2 : posicion1;
+  for (int i = 0; i < mayor; i++) {
+    if (i == menor) nodoA = lista;
+    if (i == mayor) nodoB = lista;
+    lista = lista->sig;
+  }
+  /* ahora nodoA apunta al nodo en la menor posicion y nodoB al nodo en la posicion mayor */
+  
+  GNodo *auxA = nodoA;
+  nodoA->sig = nodoB->sig;
+  nodoA->ant = nodoB->ant;
+  (nodoB->sig)->ant = nodoA;
+  (nodoB->ant)->sig = nodoA;
+  nodoB->sig = auxA->sig;
+  nodoB->ant = auxA->ant;
+  (auxA->sig)->ant = nodoB;
+  (auxA->ant)->sig = nodoB;
+
+  return inicio;
 }
--- gdclist.h
+++ gdclist.h
@@ -3,8 +3,6 @@
 
 #include <stddef.h>
 
-typedef int (*FuncionDeComparacion) (void * dato1, void * dato2); // devuelve 1 en caso de que sean iguales o 0 si no lo son
-
 
 typedef struct _GNodo {
   void *dato;
@@ -12,7 +10,7 @@ typedef struct _GNodo {
   struct _GNodo *ant;
 } GNodo;
 
-typedef GNodo * GList;
+typedef GNodo *GList;
 
 // Devuelve una lista vacía.
 GList gdclist_crear();
@@ -23,8 +21,14 @@ void gdclist_destruir(GList lista);
 // Determina si la lista es vacía.
 int gdclist_vacia(GList lista);
 
-DNodo * cdlist_agregar_inicio(DNodo * lista, int dato);
+// Agrega un elemento al inicio de la lista. 
+GNodo *gdclist_agregar_inicio(GNodo *lista, void *dato);
 
-DNodo * cdlist_agregar_final(DNodo * lista, int dato);
+// Agrega un elemento al final de la lista.
+GNodo *gdclist_agregar_final(GNodo *lista, void *dato);
+
+// Dada una lista y dos posiciones, intercambia los nodos en posicion1 y posicion2 entre si.
+// Asumimos que las posiciones van de 0 hasta longitud-1 y posicion1 != posicion2.
+GNodo *gdclist_intercambiar(GNodo *lista, int posicion1, int posicion2);
 
 #endif /* __GDCLIST_H */
