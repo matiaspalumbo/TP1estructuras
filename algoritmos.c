dd2a6ff7a59a948ddfb875e55e1842b657998ce1
Matthew Evans <Mati@Matiass-MacBook-Pro.local>
2020-04-24 16:30:52 -0300

    added pseudocode for merge sort

--- algoritmos.c
+++ algoritmos.c
@@ -45,7 +45,6 @@ int compNumeros(void* num1, void* num2) {
 //   return lista;
 // }
 
-
 // the best (i think) shot at selection sort
 GList selectionSort(GList lista, FuncionComparadora comparar) {
   if (!gdclist_es_vacia(lista)) {
@@ -88,4 +87,54 @@ GList insertionSort(GList lista, FuncionComparadora comparar) {
   }
   return lista;
 }
+
+
+// MERGE SORT
+// function merge(left, right) is
+//     var result := empty list
+
+//     while left is not empty and right is not empty do
+//         if first(left) ≤ first(right) then
+//             append first(left) to result
+//             left := rest(left)
+//         else
+//             append first(right) to result
+//             right := rest(right)
+
+//     // Either left or right may have elements left; consume them.
+//     // (Only one of the following loops will actually be entered.)
+//     while left is not empty do
+//         append first(left) to result
+//         left := rest(left)
+//     while right is not empty do
+//         append first(right) to result
+//         right := rest(right)
+//     return result
+
+// function merge_sort(node head) is
+//     // return if empty list
+//     if head = nil then
+//         return nil
+//     var node array[32]; initially all nil
+//     var node result
+//     var node next
+//     var int  i
+//     result := head
+//     // merge nodes into array
+//     while result ≠ nil do
+//          next := result.next;
+//          result.next := nil
+//          for(i = 0; (i < 32) && (array[i] ≠ nil); i += 1) do
+//               result := merge(array[i], result)
+//               array[i] := nil
+//          // do not go past end of array
+//          if i = 32 then
+//                i -= 1
+//          array[i] := result
+//          result := next
+//     // merge array into single list
+//     result := nil
+//     for (i = 0; i < 32; i += 1) do
+//          result := merge(array[i], result)
+//     return result
 
